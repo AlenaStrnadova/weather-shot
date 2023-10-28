d86b71cf31a086ca21dc1cec66cf8577bce6f40f
AlenaStrnadova <a1_strnadova@utb.cz>
2023-10-28 21:03:38 +0200

    adding a photo to the gallery feature

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -19,6 +19,7 @@ import { camera, trash, close } from 'ionicons/icons';
 
 import { usePhotoGallery } from '../hooks/usePhotoGallery';
 
+const { photos, takePhoto } = usePhotoGallery();
 
 const Tab2: React.FC = () => {
   
@@ -32,6 +33,17 @@ const Tab2: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={photo.filepath}>
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+        
+        
         <IonFab vertical="bottom" horizontal="center" slot="fixed">
           <IonFabButton onClick={() => takePhoto()}>
             <IonIcon icon={camera}></IonIcon>
